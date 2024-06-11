37cf8a3aac4ae9ae3e265299dd9436c6a3ce3607
Viet Anh Duong Nguyen <98511848+vanhsusu03@users.noreply.github.com>
2024-06-12 00:09:57 +0700

    update chart about sales

--- src/screens/admin/SaleScreen.js
+++ src/screens/admin/SaleScreen.js
@@ -13,11 +13,30 @@ import { fetchPaymentData } from '../../utils/firestore.js'
 import { FontAwesome6, FontAwesome5 } from '@expo/vector-icons'
 import { SafeAreaView, Dimensions } from 'react-native'
 import { LineChart } from 'react-native-chart-kit'
+
+import DropdownSelect from '../../components/DropdownSelect.js'
+
 const AdminSaleScreen = ({ navigation }) => {
   const [paymentList, setPaymentList] = useState([])
-  const [day, setDay] = useState([])
-  const [sale, setSale] = useState([])
-  const [selectedData, setSelectedData] = useState(null)
+  const [day, setDay] = useState(['1', '2', '3'])
+  const [sale, setSale] = useState([0, 0, 0])
+
+  const [day_w, setDayW] = useState([])
+  const [sale_w, setSaleW] = useState([])
+
+  const [day_m, setDayM] = useState([])
+  const [sale_m, setSaleM] = useState([])
+
+  const [day_y, setDayY] = useState([])
+  const [sale_y, setSaleY] = useState([])
+
+  let selectedOption = 'Trong tuần'
+  const options = ['Trong tuần', 'Trong tháng', 'Trong năm']
+
+  const handleSelect = option => {
+    selectedOption = option
+    filterSales(selectedOption)
+  }
 
   const mergeAndSortDataByDate = data => {
     const dateMap = {}
@@ -44,21 +63,20 @@ const AdminSaleScreen = ({ navigation }) => {
     try {
       let newData = await fetchPaymentData()
 
+      let newDataCopy_w = JSON.parse(JSON.stringify(newData))
+      let newDataCopy_m = JSON.parse(JSON.stringify(newData))
+      let newDataCopy_y = JSON.parse(JSON.stringify(newData))
       newData = mergeAndSortDataByDate(newData)
+
+      newDataCopy_w = mergeAndSortDataByDate(newDataCopy_w)
+      newDataCopy_m = mergeAndSortDataByDate(newDataCopy_m)
+      newDataCopy_y = mergeAndSortDataByDate(newDataCopy_y)
 
       setPaymentList(newData)
 
-      newData.forEach(element => {
-        setSale(sale => [...sale, element.total / 1000])
-        setDay(day => [...day, element.date.toString()])
-      })
-
-      const processedDay = newData.map(item => {
-        const [day, month, year] = item.date.split('/')
-        return `${day}/${month}`
-      })
-
-      setDay(processedDay)
+      weekFormat(newDataCopy_w)
+      monthFormat(newDataCopy_m)
+      yearFormat(newDataCopy_y)
     } catch (error) {
       console.error('Error reloading data:', error)
     }
@@ -83,6 +101,166 @@ const AdminSaleScreen = ({ navigation }) => {
     return reload
   }, [navigation, reloadPayment])
 
+  const weekFormat = ndt => {
+    setSaleW([])
+    setDayW([])
+    const curDate = new Date().getDate()
+    const curMonth = new Date().getMonth() + 1
+    for (let i = ndt.length - 1; i >= 0; i--) {
+      const element = ndt[i]
+      const [dayString, month, year] = element.date.split('/')
+      const day = parseInt(dayString, 10)
+      const m = parseInt(month, 10)
+      if (m != curMonth || day < curDate - 6) {
+        ndt.splice(i, 1)
+      }
+    }
+
+    for (let i = 6; i >= 0; i--) {
+      var dayy = curDate - i
+      var dayyString = dayy.toString()
+      if (dayy / 10 == 0) {
+        dayyString = '0' + dayyString
+      }
+      setDayW(day_w => [...day_w, dayyString + '/' + curMonth])
+    }
+    var zero = 0,
+      one = 0,
+      two = 0,
+      three = 0,
+      four = 0,
+      five = 0,
+      six = 0 // distance from curDate
+    ndt.forEach(element => {
+      const [dayString, monthString, year] = element.date.split('/')
+      const day = parseInt(dayString, 10)
+      if (day == curDate - 6) {
+        six = element.total / 1000000
+      } else if (day == curDate - 5) {
+        five = element.total / 1000000
+      } else if (day == curDate - 4) {
+        four = element.total / 1000000
+      } else if (day == curDate - 3) {
+        three = element.total / 1000000
+      } else if (day == curDate - 2) {
+        two = element.total / 1000000
+      } else if (day == curDate - 1) {
+        one = element.total / 1000000
+      } else if (day == curDate) {
+        zero = element.total / 1000000
+      }
+    })
+    setSaleW([six, five, four, three, two, one, zero])
+  }
+
+  const monthFormat = ndt => {
+    setSaleM([])
+    setDayM([])
+    var curMonth = new Date().getMonth() + 1
+    const curYear = new Date().getFullYear()
+    let sundays = []
+    for (let i = ndt.length - 1; i >= 0; i--) {
+      const element = ndt[i]
+      const [day, monthString, year] = element.date.split('/')
+      const month = parseInt(monthString, 10)
+
+      if (month !== curMonth) {
+        ndt.splice(i, 1)
+      }
+    }
+
+    const daysInMonth = new Date(curYear, curMonth, 0).getDate()
+    for (let day = 1; day <= daysInMonth; day++) {
+      const date = new Date(curYear, curMonth, day)
+      if (date.getDay() == 2) {
+        // 0 corresponds to Sunday
+        setDayM(day_m => [...day_m, day.toString() + '/' + curMonth.toString()])
+        sundays.push(day)
+      }
+    }
+    let totals = new Array(sundays.length).fill(0)
+
+    ndt.forEach(element => {
+      const [dayString, month, year] = element.date.split('/')
+      const day = parseInt(dayString, 10)
+      if (day <= sundays[0]) {
+        totals[0] += element.total / 1000000
+      } else if (day <= sundays[1]) {
+        totals[1] += element.total / 1000000
+      } else if (day <= sundays[2]) {
+        totals[2] += element.total / 1000000
+      } else if (day <= sundays[3]) {
+        totals[3] += element.total / 1000000
+      }
+      if (sundays.length === 5 && day > sundays[3]) {
+        totals[4] += element.total / 1000000
+      }
+    })
+    setSaleM(totals)
+  }
+
+  const yearFormat = ndt => {
+    setDayY([])
+    setSaleY([])
+    var curYear = new Date().getFullYear()
+
+    for (let i = ndt.length - 1; i >= 0; i--) {
+      const element = ndt[i]
+      const [day, month, yearString] = element.date.split('/')
+      const year = parseInt(yearString, 10)
+
+      if (year !== curYear) {
+        ndt.splice(i, 1)
+      }
+    }
+
+    var jan = 0,
+      feb = 0,
+      mar = 0,
+      apr = 0,
+      may = 0,
+      jun = 0,
+      jul = 0,
+      aug = 0,
+      sep = 0,
+      oct = 0,
+      nov = 0,
+      dec = 0
+
+    ndt.forEach(element => {
+      const [d, m, y] = element.date.split('/')
+      const month = parseInt(m, 10)
+      if (month == 1) {
+        jan += element.total / 1000000
+      } else if (month == 2) feb += element.total / 1000000
+      else if (month == 3) mar += element.total / 1000000
+      else if (month == 4) apr += element.total / 1000000
+      else if (month == 5) may += element.total / 1000000
+      else if (month == 6) jun += element.total / 1000000
+      else if (month == 7) jul += element.total / 1000000
+      else if (month == 8) aug += element.total / 1000000
+      else if (month == 9) sep += element.total / 1000000
+      else if (month == 10) oct += element.total / 1000000
+      else if (month == 11) nov += element.total / 1000000
+      else if (month == 12) dec += element.total / 1000000
+    })
+    setSaleY([jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec])
+    setDayY([
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ])
+  }
+
   const renderPayment = ({ item }) => {
     return (
       <TouchableOpacity style={styles.paymentItem}>
@@ -92,22 +270,24 @@ const AdminSaleScreen = ({ navigation }) => {
             <Text style={styles.paymentId}>{item.date}</Text>
           </View>
           <View style={styles.totalContainer}>
-            <Text style={styles.paymentId}>{item.total/1000}.000vnd</Text>
+            <Text style={styles.paymentId}>{item.total / 1000}.000vnd</Text>
           </View>
         </View>
       </TouchableOpacity>
     )
   }
 
-  const handleDataPointClick = data => {
-    setSelectedData({
-      value: data.value,
-      x: data.x,
-      y: data.y
-    })
-    setTimeout(() => {
-      setSelectedData(null)
-    }, 2000)
+  const filterSales = selectedOption => {
+    if (selectedOption === 'Trong tuần') {
+      setSale(sale_w)
+      setDay(day_w)
+    } else if (selectedOption === 'Trong tháng') {
+      setDay(day_m)
+      setSale(sale_m)
+    } else if (selectedOption === 'Trong năm') {
+      setSale(sale_y)
+      setDay(day_y)
+    }
   }
 
   const renderChart = () => {
@@ -124,7 +304,7 @@ const AdminSaleScreen = ({ navigation }) => {
           }}
           width={Dimensions.get('window').width - 20}
           height={220}
-          yAxisSuffix='k'
+          yAxisSuffix='tr'
           yAxisInterval={1}
           chartConfig={{
             backgroundColor: '#e26a00',
@@ -147,7 +327,6 @@ const AdminSaleScreen = ({ navigation }) => {
             marginVertical: 8,
             borderRadius: 16
           }}
-          
         />
       </View>
     )
@@ -159,6 +338,9 @@ const AdminSaleScreen = ({ navigation }) => {
       <View style={styles.listTitleContainer}>
         <Text style={styles.listTitle}>Biểu đồ thống kê doanh thu</Text>
       </View>
+      <TouchableOpacity style={styles.add}>
+        <DropdownSelect options={options} onSelect={handleSelect} />
+      </TouchableOpacity>
       <View style={styles.chartContainer}>{renderChart()}</View>
       <View style={styles.listTitleContainer}>
         <Text style={styles.listTitle}>Doanh thu chi tiết</Text>
@@ -185,7 +367,7 @@ const styles = StyleSheet.create({
   },
   totalContainer: {
     alignItems: 'center',
-    justifyContent: 'left',
+    justifyContent: 'left'
   },
   header: {
     textAlign: 'center',
@@ -275,5 +457,26 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: colors.text1
   },
+  add: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingRight: 30,
+    marginTop: -10,
+    zIndex: 3, // works on ios
+    elevation: 3 // works on android
+  },
+  icon: {
+    marginLeft: 5,
+    color: '#fff',
+    backgroundColor: '#EE9C37',
+    fontSize: 17,
+    padding: 2,
+    marginRight: 8
+  },
+  text: {
+    fontSize: 18,
+    color: '#EE9C37'
+  }
 })
 export default AdminSaleScreen
